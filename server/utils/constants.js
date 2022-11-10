2a02607513a519a154491d41e3a18cb17313e484
BarakBarzilay <barakoyb@gmail.com>
2022-11-10 12:33:33 +0200

    changed port

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const {
   HARPERDB_URL,
   HARPERDB_PW,
-  PORT = 4000,
+  PORT = process.env.PORT || 4000,
 } = process.env;
 
 const CHAT_BOT = 'ChatBot';
 
 module.exports = {
-  PORT, HARPERDB_URL,
-  HARPERDB_PW, CHAT_BOT
+  PORT,
+  HARPERDB_URL,
+  HARPERDB_PW,
+  CHAT_BOT,
 };
