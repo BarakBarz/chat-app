65087972764a5c7d07a7fb68827cb48d368d5d58
BarakBarzilay <barakoyb@gmail.com>
2022-11-13 13:28:24 +0200

    "fix several items not showing on screen AGAIN"
    This reverts commit beefc44e8c4148c0d3deaee96763e48e443ba601.

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,7 @@ import Chat from './pages/Chat/Chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:3000'); // development
-// const socket = io.connect('https://bb-chat-app.herokuapp.com/'); // production
+const socket = io.connect('https://bb-chat-app.herokuapp.com/');
 
 function App() {
   const [username, setUsername] = useState('');
